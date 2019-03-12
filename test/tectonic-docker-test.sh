ab305bbadf3f5a543798e31799826938685b47fc
Thomas <hollandpirates@gmail.com>
2019-03-12 07:11:20 +0100

    Revert "Try the tectonic workaround again"
    This reverts commit d04f3ffe

--- test/tectonic-docker-test.sh
+++ test/tectonic-docker-test.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-sudo mkdir -p ~/.config/Tectonic/
-echo "[[default_bundles]]" | sudo tee --append ~/.config/Tectonic/config.toml
-sudo echo "url = \"https://tectonic.newton.cx/bundles/tlextras-2018.1r0/bundle.tar\"" | sudo tee --append ~/.config/Tectonic/config.toml
-
 # Copied from 1-tectonic-docker/.travis.yml without comments
-docker run --mount src=$TRAVIS_BUILD_DIR/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic --keep-intermediates --reruns 0 biber-mwe.tex; biber biber-mwe; tectonic biber-mwe.tex; tectonic main.tex"
+docker run --mount src=$TRAVIS_BUILD_DIR/src,target=/usr/src/tex,type=bind dxjoke/tectonic-docker /bin/sh -c "tectonic --keep-intermediates --reruns 0 biber-mwe.tex; biber biber-mwe; biber-mwe.tex; tectonic main.tex"
