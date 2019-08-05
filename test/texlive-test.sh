0152518a65d7177fa6aaaa4a6e6de3a66a6a7aec
Pablo González L <pablgonz@yahoo.com>
2019-08-05 02:35:55 -0400

    Extending test

--- test/texlive-test.sh
+++ test/texlive-test.sh
@@ -2,14 +2,14 @@
 
 # This file is used for testing this repo, replacing the corresponding .travis.yml which cannot be included.
 
-#sudo apt-get install python-pygments
-
 cd ${TRAVIS_BUILD_DIR}/4-texlive/
 source ${TRAVIS_BUILD_DIR}/4-texlive/texlive/texlive_install.sh
 
 cd ${TRAVIS_BUILD_DIR}/src/
 texliveonfly ./main.tex
 
+texliveonfly ./biber-mwe.tex
+
 texliveonfly fonts.tex --compiler=lualatex
 
-#texliveonfly --arguments='-shell-escape' ./minted.tex
+texliveonfly --arguments='-shell-escape' ./minted.tex
